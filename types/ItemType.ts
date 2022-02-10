3a527680043c8f3b653db5266da29b740c1cefcb
Lukas Singer <lukas.singer@outlook.com>
2022-02-10 17:59:29 +0100

    Add Item schema and ItemType to project

---
+++ types/ItemType.ts
@@ -0,0 +1,12 @@
+interface ItemType{
+  id: number;
+  itemName: string;
+  protein: number;
+  fat: number;
+  carb: number;
+  caloriesPerG: number;
+  caloriesPerMl: number;
+  caloriesPerEl: number;
+}
+
+export {ItemType};
