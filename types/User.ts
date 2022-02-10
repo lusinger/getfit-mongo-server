77786e5c524d6f2ad6fca884bae0f0246e8ea72c
Lukas Singer <lukas.singer@outlook.com>
2022-02-10 17:38:04 +0100

    Create UserSchema for later use in mongoDB
    -Add related UserType TS interface for data handling

---
+++ types/User.ts
@@ -0,0 +1,16 @@
+interface UserType{
+  id: number;
+  userName: string;
+  mail: string;
+  password: string;
+  fullName?: string;
+  age: number;
+  height: number;
+  currentWeight: number;
+  targetWeight: number;
+  changePerWeek: 1.2 | 1.375 | 1.55 | 1.725 | 1.9;
+  gender: 'male' | 'female';
+  calorieGoal: number;
+}
+
+export {UserType};
