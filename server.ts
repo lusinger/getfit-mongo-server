8bb7d8c1c41b21f62de9fdc80e9a379edcc63ad9
Lukas Singer <lukas.singer@outlook.com>
2022-02-10 16:50:51 +0100

    Initial commit
    -Add basic node packages dotenv, express, nodemon, chalk
    -Create basic express server
    -Create .gitignore file

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = express();
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log(`${chalk.yellowBright('[SERVER]')} listening on port ${chalk.yellowBright(port)}`);
+})
